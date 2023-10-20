5e7c085b7f63487b3c9fa7ea145b6f85239f1c54
SLYVYNET <sofiialyvynets@otto.de>
2023-10-20 09:45:02 +0200

    Adding IoC

---
+++ src/main/java/com/example/spring_course/Person.java
@@ -0,0 +1,13 @@
+package com.example.spring_course;
+
+public class Person {
+    private Pet pet;
+    public Person(Pet pet){
+        this.pet = pet;
+    }
+    public void callYourPet (){
+        System.out.println("эй, псина");
+        pet.say();
+
+    }
+}
